50683823631f424b10ca45e47b5bed885c9437cb
SaaberDev <saaberdev@github.com>
2021-02-05 09:50:03 +0600

    newsletter delete completed

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -13,4 +13,26 @@ class Subscriber extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    const SUBSCRIBER = 1;
+    const NOT_SUBSCRIBER = 0;
+
+    public function isSubscriber()
+    {
+        return $this->where('subscriber_status', '=', self::SUBSCRIBER);
+    }
+
+    public function scopeFilterBy($query, $column, $arg)
+    {
+        return $query->where(function ($query) use ($arg, $column) {
+            $arg == '' ? $query->orderBy('id', 'desc') : $query->orWhere($column, '=', $arg);
+        });
+    }
+
+    public function scopeSearchBy($query, $column, $search)
+    {
+        return $query->where(function ($query) use ($search, $column) {
+            $query->where($column, 'like', '%' . $search . '%');
+        });
+    }
 }
