24784db9ded0fe2df18156c471a513897e62d420
SaaberDev <saaberdev@gmail.com>
2021-02-22 06:19:11 +0600

    configuring laravel breeze authentication

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegistrationRequest;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(RegistrationRequest $request)
+    {
+        Auth::login($user = User::create([
+            'name' => $request->input('name'),
+            'username' => last(explode(' ', strtolower($request->input('name')))),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]));
+
+        event(new Registered($user));
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
