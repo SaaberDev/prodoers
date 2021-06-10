6c773699bb15274d5d4a16c6fea4dbc8218562c1
SaaberDev <saaberdev@gmail.com>
2021-06-10 12:33:26 +0600

    [related service] - fixed related services bug

--- app/Http/Controllers/Guest/Service/GuestServiceController.php
+++ app/Http/Controllers/Guest/Service/GuestServiceController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Guest\Service;
 
 use App\Http\Controllers\Controller;
 use App\Models\Service;
+use App\Models\Tag;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -20,18 +22,14 @@ class GuestServiceController extends Controller
     public function index($service_slug)
     {
         $services = Service::getSlug($service_slug)->first();
-//        $services = Service::getSlug($service_slug)->with(['serviceCategories' => function($q){
-//            $q->with('serviceCategoryFaqs');
-//        }])->first();
-        $related_services = Service::hideCurrent($services)->getAllPublished()->whereHas('service_tags', function($query) use ($services) {
-            $query->whereIn('service_tags.id', $services->service_tags);
-        })->get();
-//        dd($related_services);
-        request()->session()->put('url.intended', route('guest.order.index', $services->slug));
+        $related_services = Service::hideCurrent($services)->getAllPublished()->whereHas('tags', function(Builder $query) use ($services) {
+            $query->select('tags.id')->whereIn('tags.id', $services->tags->pluck('id'));
+        })->select(['title', 'slug', 'price', 'thumbnail'])->get();
 
         $service_image = \Storage::disk('local')->url(config('designwala_paths.images.services.service_image'));
         $service_thumbnail = \Storage::disk('local')->url(config('designwala_paths.images.services.thumbnail'));
 
+        request()->session()->put('url.intended', route('guest.order.index', $services->slug));
         return view('guest.pages.service_show', compact('services', 'related_services', 'service_image', 'service_thumbnail'));
     }
 }
