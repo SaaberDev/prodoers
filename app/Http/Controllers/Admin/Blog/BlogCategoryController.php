92b920154cb29041fafc9bae51d43b2e9e596e69
SaaberDev <saaberdev@gmail.com>
2022-03-07 00:06:07 +0600

    [auth] - added some improvement

--- app/Http/Controllers/Admin/Blog/BlogCategoryController.php
+++ app/Http/Controllers/Admin/Blog/BlogCategoryController.php
@@ -87,3 +87,8 @@ class BlogCategoryController extends Controller
         //
     }
 }
+
+
+
+
+
--- routes/user.php
+++ routes/user.php
@@ -2,7 +2,7 @@
 
     Route::name('client.')
         ->middleware([
-            // 'auth', 'verified'
+//             'auth', 'verified'
         ])
         ->group(function () {
 
