4799d264d8c09e879a7daad982e0388019835284
SaaberDev <saaberdev@gmail.com>
2022-03-08 01:50:06 +0600

    [ci/cd] - added laravel.yml configuration

--- resources/views/admin_panel/includes/header.blade.php
+++ resources/views/admin_panel/includes/header.blade.php
@@ -78,7 +78,9 @@
                                 <a class="dropdown-item" href="#">{{ Auth::user()->name ?? '' }}</a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#">Profile</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
+{{--                                <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>--}}
+                                <form method="POST" action="{{ route('logout') }}" id="logout">@csrf</form>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout').submit();">Logout</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown v-center">
