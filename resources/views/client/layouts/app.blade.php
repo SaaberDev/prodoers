3daad063d9c170363876cfca183ef1ea14549114
TasnimAhmed <tasnim.ahamed.20@gmail.com>
2022-03-19 19:41:22 +0600

    mix font weight problem solve

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -6,8 +6,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Designwala</title>
-    
-    <!--Media CSS files-->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!--All CSS files-->
     <link rel="stylesheet" href="{{ asset('_assets/_client/css/main.css') }}">
 
     @stack('styles')
